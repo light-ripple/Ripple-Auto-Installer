e1aa9de36318f3e19781c27f745b26ced2c57b74
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-11-09 23:20:24 +0600

    convert spaces to tabs && fix incorrect alignments

--- Main/tmux.sh
+++ Main/tmux.sh
@@ -5,7 +5,7 @@
 : '
 -----------------------------------------------------------------------------------------------
 |  Created by LiveEmily <d.claassen2003@outlook.com> in 2020 under the terms of GNU AGPL-3.0  |
-|             Last Updated on Monday, November 9, 2020 at 03:25 PM (CET)                      |
+|               Last Updated on Monday, November 9, 2020 at 11:20 PM (GMT+6)                  |
 -----------------------------------------------------------------------------------------------
 '
 
@@ -20,7 +20,7 @@
 '
 
 # Version #
-UPSTREAM_VERSION="1.0.1"
+UPSTREAM_VERSION="1.0.4"
 
 # Name
 session="Ripple"
@@ -32,43 +32,46 @@ TMUX_SH="https://raw.githubusercontent.com/Uniminin/Ripple-Auto-Installer/master
 # tmux.sha1 (checksum)
 TMUX_SHA1="https://raw.githubusercontent.com/Uniminin/Ripple-Auto-Installer/master/Main/tmux.sha1"
 
-# Command Overwrites
-alias EXIT="exit"
-alias READ="read -r"
 
 # Colors For Prints
 alias RPRINT="printf '\\033[0;31m%s\\n''\\033[0;37m'"     # Red
 alias GPRINT="printf '\\033[0;32m%s\\n''\\033[0;37m'"     # Green
+
+
+# Command Overwrites
+alias EXIT="exit"
+alias READ="read -r"
+
 
 checksum_checker="true"
 
 # Simplified File Integrity Checker by uniminin <uniminin@zoho.com> under the terms of AGPLv3
 # CHECK FILE INTEGRITY
 if [ "$checksum_checker" = "true" ]; then
-    if [ ! -f "tmux.sha1" ]; then
-        RPRINT "file integrity data not found" ; GPRINT "Fetching the latest file integrity data"
-        wget -O "tmux.sha1" "$TMUX_SHA1"
-        if [ ! -f "tmux.sha1" ]; then
-            RPRINT "Failed to fetch the latest file integrity data" ; EXIT 1
-        fi
-    fi
+	if [ ! -f "tmux.sha1" ]; then
+		RPRINT "file integrity data not found" ; GPRINT "Fetching the latest file integrity data"
+		wget -O "tmux.sha1" "$TMUX_SHA1"
+		if [ ! -f "tmux.sha1" ]; then
+			RPRINT "Failed to fetch the latest file integrity data" ; EXIT 1
+		fi
+	fi
 
-    if [ -f "tmux.sha1" ]; then
-        sha1sum -c tmux.sha1 || match="false"
-        if [ "$match" = "false" ]; then
-            GPRINT "Fetching the latest script, please try again..."
-            wget -O "tmux.sh" "$TMUX_SH"
-            EXIT 1
-        fi
-    else
-        RPRINT "file integrity data not found" ; EXIT 1
-    fi
+	if [ -f "tmux.sha1" ]; then
+		sha1sum -c tmux.sha1 || match="false"
+		if [ "$match" = "false" ]; then
+			GPRINT "Fetching the latest script, please try again..."
+			wget -O "tmux.sh" "$TMUX_SH"
+			EXIT 1
+		fi
+	else
+		RPRINT "file integrity data not found" ; EXIT 1
+	fi
 fi
 
 
 while [ "$#" -ge 0 ]; do case "$1" in
 	"--new" | "-N")
-	        if command -v tmux >/dev/null; then
+		if command -v tmux >/dev/null; then
 			GPRINT "Directory: "
 			READ directory
 			tmux new-session -c /"$USER"/"$directory"/ -s "$session" -d
@@ -78,11 +81,11 @@ while [ "$#" -ge 0 ]; do case "$1" in
 			tmux send-keys "exec hanayo/hanayo" C-m
 			tmux send-keys "exec api/api" C-m
 		else
-                        RPRINT "Fatal: tmux is not executable on this system!"
-                        EXIT 1
-                fi
+			RPRINT "Fatal: tmux is not executable on this system!"
+			EXIT 1
+		fi
 		EXIT 0 ;;
-	
+
 	"--attach" | "-A")
 		tmux attach-session -t "$session"
 		EXIT 0 ;;
