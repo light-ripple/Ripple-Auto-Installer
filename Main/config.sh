0bc14bcb3456a430d5c257d99886846839cc42c4
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-12-14 12:17:39 +0600

    Create config.sh

---
+++ Main/config.sh
@@ -0,0 +1,22 @@
+# --Configuration-- #
+# Note: If left empty it'll capture input at script execution time with confirmation
+
+# Master Directory::contains::Ripple stack software(s)
+targetDir=""
+
+# Server Domain
+domain=""
+
+# cikey (Required by pep.py). Hint: can be random like password, not necessarily anything specific.
+cikey=""
+
+# OSU!API Key (https://old.ppy.sh/p/api). Note: Requires OSU! Account.
+api=""
+
+# API Secret. Hint: can be random like password, not necessarily anything specific.
+api_secret=""
+
+# Database (Mysql)
+mysql_user=""
+mysql_password=""
+database_name=""
